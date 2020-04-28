d2cbb81b74adbe51d743ef68359b942b4f8b9fa6
Olha10 <100kravchenko@gmail.com>
2020-04-28 14:34:13 +0300

    Create tests in YourProfilePageTests class, add locators to YourProfilePage class

--- src/main/java/pages/YourProfilePage.java
+++ src/main/java/pages/YourProfilePage.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.By;
 public class YourProfilePage {
 
     public By yourProfileButton = (By.cssSelector("[data-ga-click='Header, go to profile, text:your profile']"));
-    public By overviewProfileButton = (By.cssSelector("[class='UnderlineNav-body'] [aria-current='page']"));
-    public By repositoriesButton = (By.cssSelector("[class='UnderlineNav-body'] [href='/Olha10?tab=repositories']"));
+    public By overviewProfileNavButton = (By.cssSelector("[class='UnderlineNav-body'] [aria-current='page']"));
+    public By repositoriesProfileNavButton = (By.cssSelector("[class='UnderlineNav-body'] [href='/Olha10?tab=repositories']"));
     public By editProfileButton = (By.cssSelector("[class='d-none d-md-block'] [class='hide-sm hide-md']"));
-
-
-
-
+    public By projectsProfileNavButton = (By.cssSelector("[aria-label='User profile'] [href='/Olha10?tab=projects'] "));
+    public By packagesProfileNavButton = (By.cssSelector("[aria-label='User profile'] [href='/Olha10?tab=packages']"));
+    public By starsProfileNavButton = (By.cssSelector("[aria-label='User profile'] [href='/Olha10?tab=stars']"));
+    public By followersProfileNavButton = (By.cssSelector("[aria-label='User profile'] [href='/Olha10?tab=followers']"));
+    public By followingProfileNavButton = (By.cssSelector("[href='/Olha10?tab=following']"));
 
 }
--- src/test/java/YourProfilePageTests.java
+++ src/test/java/YourProfilePageTests.java
@@ -3,18 +3,39 @@ import org.testng.annotations.Test;
 
 public class YourProfilePageTests extends LogInPageTests {
     @Test(priority = 1, groups = "gitHubPages_tests")
-    public void yourProfilePage() {
+    public void yourProfileNavButtons() {
 
         elementsHelper.clickOnVisibleAndClickableElement(headerTabs.profileAndMoreMenuButton,5);
         elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.yourProfileButton,5);
 
-        Assert.assertTrue(elementsHelper.isElementClickable(yourProfilePage.overviewProfileButton,1), "OverviewButton should be clickable");
-        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.overviewProfileButton, 1), "Overview");
+        elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.overviewProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.overviewProfileNavButton, 5), "Overview");
 
-        Assert.assertTrue(elementsHelper.isElementClickable(yourProfilePage.repositoriesButton,1), "repositoriesButton should be clickable");
-        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.repositoriesButton, 1), "Repositories 1");
+        elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.repositoriesProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.repositoriesProfileNavButton, 5), "Repositories 1");
 
-        Assert.assertTrue(elementsHelper.isElementClickable(yourProfilePage.editProfileButton,1), "editProfileButton should be clickable");
-        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.editProfileButton, 1), "Edit profile");
+       elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.projectsProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.projectsProfileNavButton, 5), "Projects 0");
+
+        elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.packagesProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.packagesProfileNavButton, 5), "Packages 0");
+
+        elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.starsProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.starsProfileNavButton, 5), "Stars 0");
+
+        elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.followersProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.followersProfileNavButton, 5), "Followers 0");
+
+        elementsHelper.clickOnVisibleAndClickableElement(yourProfilePage.followingProfileNavButton,5);
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.followingProfileNavButton, 5), "Following 0");
+
+    }
+
+    @Test (priority = 2, groups = "gitHubPages_tests")
+    public void yourProfileButtons (){
+
+        Assert.assertTrue(elementsHelper.isElementClickable(yourProfilePage.editProfileButton,5), "editProfileButton should be clickable");
+        Assert.assertEquals(elementsHelper.getElementTextVisibilityOf(yourProfilePage.editProfileButton, 5), "Edit profile");
+
     }
 }
